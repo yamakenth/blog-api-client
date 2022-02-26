f0524313831af47bbaa904f2997f354716b36987
Ken Yamada <yamakenth@gmail.com>
2022-02-26 11:18:52 +0700

    Add semantic HTML

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,17 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function NavBar() {
   return (
-    <Navbar bg='dark' variant='dark' expand='sm' className='fixed-top'>
-      <Container>
-        <Navbar.Brand href='/'>Blog</Navbar.Brand>
-        <Nav>
-          <Nav.Link href='/'><i className='bi bi-person-fill'>&#9;Dummy</i></Nav.Link>
-          <Nav.Link href='/'><i className='bi bi-box-arrow-in-left'>&#9;Dummy</i></Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='sm' className='fixed-top'>
+        <Container>
+          <Navbar.Brand href='/'>Blog</Navbar.Brand>
+          <Nav>
+            <Nav.Link href='/'><i className='bi bi-person-fill'>&#9;Dummy</i></Nav.Link>
+            <Nav.Link href='/'><i className='bi bi-box-arrow-in-left'>&#9;Dummy</i></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
