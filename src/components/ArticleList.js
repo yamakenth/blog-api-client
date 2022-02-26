56dd068affd6a3b53030338d4a5dc0916c39d186
Ken Yamada <yamakenth@gmail.com>
2022-02-26 23:08:47 +0700

    Adjust margin-top of main content

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import _ from 'underscore';
 import moment from 'moment';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 function ArticleList() {
   const [articles, setArticles] = useState([]);
