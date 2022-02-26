7b3328bd0bfdf73a0764aa102a45b18421eebc3c
Ken Yamada <yamakenth@gmail.com>
2022-02-26 22:31:59 +0700

    Add ability to display all articles

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,34 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import _ from 'underscore';
+import moment from 'moment';
+
 function ArticleList() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:1000/blog/articles')
+      .then((res) => {
+        setArticles(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  
   return (
     <div>
-      <h1>Article 1</h1>
-      <h1>Article 2</h1>
-      <h1>Article 3</h1>
-      <h1>Article 4</h1>
-      <h1>Article 5</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article</h1>
-      <h1>Article 5</h1>
-      <h1>Article 4</h1>
-      <h1>Article 3</h1>
-      <h1>Article 2</h1>
-      <h1>Article 1</h1>
+      {
+        articles
+          // .filter((article) => article.published === true)
+          .map((article) => {
+            return (
+              <div key={article._id}>
+                <p><strong>Title:</strong> {_.unescape(article.title)}</p>
+                <p><strong>Author:</strong> {_.unescape(article.author.username)}</p>
+                <p><strong>Written on:</strong> {moment(article.createdAt).format('MMMM Do YYYY')}</p>
+                <p><strong>Published (only for testing):</strong> {String(article.published)}</p>
+                <p><strong>Text:</strong> {_.unescape(article.text)}</p>
+                <br />
+              </div>
+            );
+          })
+      }
     </div>
   );
 }
