8f64a98bd4563f9a5d1efc79ecaa3e0ecea5d024
Ken Yamada <yamakenth@gmail.com>
2022-02-27 14:42:48 +0700

    Create skeleton for comment section

--- src/components/ArticleDisplay.js
+++ src/components/ArticleDisplay.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import _ from 'underscore';
 import moment from 'moment';
 
+import CommentSection from './CommentSection';
+
 function ArticleDisplay() {
   const { id } = useParams();
 
@@ -25,15 +27,18 @@ function ArticleDisplay() {
     }, [article, id]);
 
   return (
-    <div className='mb-5'>
-      <h2>{_.unescape(article.title)}</h2>
-      <p className='text-muted mb-0'>By {_.unescape(article.author.username)}</p>
-      <p className='text-muted mb-0'>
-        <i className="bi bi-clock me-1"></i>
-        {moment(article.createdAt).format('MMMM Do YYYY')}
-      </p>
-      <hr />
-      <p>{_.unescape(article.text)}</p>
+    <div>
+      <div className='main-article'>
+        <h2>{_.unescape(article.title)}</h2>
+        <p className='text-muted mb-0'>By {_.unescape(article.author.username)}</p>
+        <p className='text-muted mb-0'>
+          <i className="bi bi-clock me-1"></i>
+          {moment(article.createdAt).format('MMMM Do YYYY')}
+        </p>
+        <hr />
+        <p>{_.unescape(article.text)}</p>
+      </div>
+      <CommentSection />
     </div>
   );
 }
---
+++ src/components/CommentSection.js
@@ -0,0 +1,9 @@
+function CommentSection() {
+  return (
+    <div>
+      <h1>This is CommentSection</h1>
+    </div>
+  );
+}
+
+export default CommentSection;
