52cedf59d2f99574f66a2342290debf555234b65
Ken Yamada <yamakenth@gmail.com>
2022-02-27 11:14:43 +0700

    Style article cards

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import _ from 'underscore';
 import moment from 'moment';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 
 function ArticleList() {
   const [articles, setArticles] = useState([]);
@@ -21,27 +21,35 @@ function ArticleList() {
   return (
     <div>
       <h4 className='mb-3'>{`Posts (${articles.length})`}</h4>
-      {
-        articles
-          // .filter((article) => article.published === true)
-          .map((article) => {
-            return (
-              <Card style={{ width: '20rem' }} key={article._id}>
-                <Card.Body>
-                  <Card.Title>{_.unescape(article.title)}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {_.unescape(article.author.username)}&nbsp;
-                    |&nbsp;&nbsp;{moment(article.createdAt).format('MMMM Do YYYY')}
-                  </Card.Subtitle>
-                  <Card.Text>
-                    {_.unescape(article.text).split(' ').slice(0, 20).join(' ') + '...'}
-                  </Card.Text>
-                  <Card.Link href="/">Read More</Card.Link>
-                </Card.Body>
-              </Card>
-            );
-          })
-      }
+      <Row>
+        {
+          articles
+            // .filter((article) => article.published === true)
+            .map((article) => {
+              return (
+                <Col sm={6} md={4} xl={3} className='mb-3 d-flex align-items-stretch'>
+                  <Card key={article._id}>
+                    <Card.Body className='d-flex flex-column justify-content-between'>
+                      <div className='mb-3'>
+                        <Card.Title>{_.unescape(article.title)}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">
+                          {_.unescape(article.author.username)}&nbsp;
+                          |&nbsp;&nbsp;{moment(article.createdAt).format('MMMM Do YYYY')}
+                        </Card.Subtitle>
+                        <Card.Text>
+                          {_.unescape(article.text).split(' ').slice(0, 20).join(' ') + '...'}
+                        </Card.Text>
+                      </div>
+                      <div>
+                        <Card.Link href="/">Read More</Card.Link>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })
+        }
+      </Row>
     </div>
   );
 }
