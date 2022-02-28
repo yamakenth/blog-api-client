4c94add3985dc3b4586883f8d8f2cd21b23a4ab1
Ken Yamada <yamakenth@gmail.com>
2022-02-28 12:02:53 +0700

    Change footer links to reflect user frontend change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,15 @@ function Footer() {
       <Navbar bg='dark' variant='dark' expand='sm'>
         <Container>
           <Nav>
-            <Nav.Link href='https://www.github.com/yamakenth' target='_blank'>
+            <Nav.Link href='https://github.com/yamakenth/blog-api-client' target='_blank'>
               <i className='bi bi-github'>&#9;GitHub</i>
             </Nav.Link>
           </Nav>
           <Navbar.Brand href='/' className='mx-0'><BrandLogo />Blog</Navbar.Brand>
           <Nav>
-            <Nav.Link href='/login'><i className='bi bi-gear-wide-connected'>&#9;Login as Admin</i></Nav.Link>
+            <Nav.Link href='https://google.com' target='_blank'>
+              <i className='bi bi-gear-wide-connected'>&#9;Admin Page</i>
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
