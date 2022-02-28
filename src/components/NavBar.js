47eace6e8b085b869e6298294936df1d8ccf6193
Ken Yamada <yamakenth@gmail.com>
2022-02-28 15:18:33 +0700

    Show different navbars depending if user is authenticated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 import BrandLogo from './BrandLogo';
 
-function NavBar() {
+function UserAuth() {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    setUsername(localStorage.getItem('username') || '');
+  }, []);
+
+  function handleClick() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    setUsername('');
+    window.location.reload();
+  }
+  
+  if (username.length > 0) {
+    return (
+      <Nav>
+        <Navbar.Text className='me-3'>
+          You're signed in as <span className='nav-username'>{username}</span>
+        </Navbar.Text>
+        <button className='logout-button' onClick={handleClick}>
+          <i className='bi bi-box-arrow-right'>&#9;Logout</i>
+        </button>
+      </Nav>
+    );
+  } else {
+    return (
+      <Nav>
+        <Nav.Link href='/signup'><i className='bi bi-person-plus'>&#9;Signup</i></Nav.Link>
+        <Nav.Link href='/login'><i className='bi bi-box-arrow-in-right'>&#9;Login</i></Nav.Link>
+      </Nav>
+    );
+  }
+}
+
+function NavBar() {  
   return (
     <header className='fixed-top'>
       <Navbar bg='dark' variant='dark' expand='sm'>
         <Container>
           <Navbar.Brand href='/'><BrandLogo />Blog</Navbar.Brand>
-          <Nav>
-            <Nav.Link href='/signup'><i className='bi bi-person-plus'>&#9;Signup</i></Nav.Link>
-            <Nav.Link href='/login'><i className='bi bi-box-arrow-in-right'>&#9;Login</i></Nav.Link>
-          </Nav>
+          <UserAuth />
         </Container>
       </Navbar>
     </header>
