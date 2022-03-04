f58a90b3b4da2ced77e8e01189134969c4fe4bac
Ken Yamada <yamakenth@gmail.com>
2022-03-04 11:35:49 +0700

    Add helper method to slice strings

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,6 +4,21 @@ import _ from 'underscore';
 import moment from 'moment';
 import { Card, Row, Col } from 'react-bootstrap';
 
+// helper method to slice strings
+function sliceStr(str, len) {
+  const oldWordCnt = str.split(' ').length;
+  
+  let shortStrArr = str.split(' ').slice(0, len);
+  const newWordCnt = shortStrArr.length;
+
+  let slicedStr = shortStrArr.join(' ');
+  if (oldWordCnt > newWordCnt) {
+    slicedStr += '...';
+  }
+  
+  return slicedStr;
+}
+
 function ArticleList() {
   const [articles, setArticles] = useState([]);
 
@@ -32,13 +47,13 @@ function ArticleList() {
                   <Card className='w-100 article-card'>
                     <Card.Body className='d-flex flex-column justify-content-between'>
                       <div className='mb-3'>
-                        <Card.Title>{_.unescape(article.title)}</Card.Title>
+                        <Card.Title>{sliceStr(_.unescape(article.title), 10)}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">
                           {_.unescape(article.author.username)}&nbsp;
                           |&nbsp;&nbsp;{moment(article.createdAt).format('MMMM Do YYYY')}
                         </Card.Subtitle>
                         <Card.Text>
-                          {_.unescape(article.text).split(' ').slice(0, 20).join(' ') + '...'}
+                          {sliceStr(_.unescape(article.text), 20)}
                         </Card.Text>
                       </div>
                       <div>
