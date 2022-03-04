282e97a2e51cc15e69d73c3a1ce425bc6402062b
Ken Yamada <yamakenth@gmail.com>
2022-03-04 11:48:09 +0700

    Change to deployed api url

--- src/components/CommentDisplay.js
+++ src/components/CommentDisplay.js
@@ -2,22 +2,23 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card } from 'react-bootstrap';
 import moment from 'moment';
+import _ from 'underscore';
 
 function CommentDisplay(props) {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:1000/blog/articles/${props.articleid}/comments`)
-      .then((res) => {
+    axios.get(`https://yamakenth-blog-api-server.herokuapp.com/api/articles/${props.articleid}/comments`)
+      .then(res => {
         setComments(res.data);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, [props.articleid]);
   
   return (
-    <div>
+    <>
       <h6>Comments ({comments.length})</h6>
       <hr className='mt-1'/>
       {
@@ -26,17 +27,17 @@ function CommentDisplay(props) {
             <Card key={comment._id} className='mb-2'>
               <Card.Body  className='py-2'>
                 <Card.Text className='text-muted mb-1'>
-                  {comment.author} | {moment(comment.createdAt).format('MMMM Do YYYY')}
+                  {_.unescape(comment.author)} | {moment(comment.createdAt).format('MMMM Do YYYY')}
                 </Card.Text>
                 <Card.Text>
-                  {comment.text}
+                  {_.unescape(comment.text)}
                 </Card.Text>
               </Card.Body>
             </Card>
           );
         })
       }
-    </div>
+    </>
   );
 }
 
